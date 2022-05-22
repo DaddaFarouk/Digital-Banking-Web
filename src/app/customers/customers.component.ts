d1b6b573a495ce709d486de3687b00fa53f3c805
DaddaFarouk <faroukdadda@gmail.com>
2022-05-22 23:40:48 +0100

    end part 3

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import {CustomerService} from "../services/customer.service";
-import {Observable} from "rxjs";
+import {catchError, map, Observable, throwError} from "rxjs";
+import {CustomerModel} from "../models/customer.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-customers',
@@ -9,12 +10,47 @@ import {Observable} from "rxjs";
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
-  customers! : Observable<any>;
+  customers! : Observable<Array<CustomerModel>>;
   errorMessage! : string;
-  constructor(private customerService:CustomerService) { }
+  searchFormGroup : FormGroup | undefined;
+  constructor(private customerService:CustomerService,
+              private fb : FormBuilder) { }
 
   ngOnInit(): void {
-    this.customers = this.customerService.getCustomers();
+    this.searchFormGroup = this.fb.group({
+        keyword : this.fb.control("")
+      });
+    this.handleSearchCustomers();
   }
 
+  handleSearchCustomers() {
+    let kw=this.searchFormGroup?.value.keyword;
+    this.customers = this.customerService.searchCustomers(kw).pipe(
+      catchError(err => {
+        this.errorMessage = err.message;
+        return throwError(err);
+      })
+    );
+  }
+
+  handleDeleteCustomer(c: CustomerModel) {
+    let conf = confirm("Are you sure ?");
+    if (!conf) return;
+    this.customerService.deleteCustomer(c.id).subscribe({
+      next : (data) => {
+        this.customers = this.customers.pipe(
+          map( data => {
+            let index = data.indexOf(c);
+            data.slice(index, 1)
+            return data;
+          })
+        );
+      },
+      error : err => {
+        console.log(err);
+      }
+    })
+  }
 }
+
+
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {CustomerModel} from "../models/customer.model";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,16 @@ export class CustomerService {
 
   constructor(private http:HttpClient) {}
 
-  public getCustomers():Observable<any> {
-    return this.http.get("http://localhost:8085/customers")
+  public getCustomers():Observable<Array<CustomerModel>> {
+    return this.http.get<Array<CustomerModel>>(environment.backendHost+"/customers")
+  }
+  public searchCustomers(keyword : string):Observable<Array<CustomerModel>> {
+    return this.http.get<Array<CustomerModel>>(environment.backendHost+"/customers/search?keyword="+keyword)
+  }
+  public saveCustomer(customer : CustomerModel):Observable<CustomerModel> {
+    return this.http.post<CustomerModel>(environment.backendHost+"/customers",customer)
+  }
+  public deleteCustomer(id : number) {
+    return this.http.delete(environment.backendHost+"/customers/"+id)
   }
 }
