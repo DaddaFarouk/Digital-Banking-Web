cb823bf58c3d29c2e2ff7c7a149b8d95f60f6a5f
DaddaFarouk <faroukdadda@gmail.com>
2022-05-22 23:39:01 +0100

    first commit

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {CustomerService} from "../services/customer.service";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+  customers! : Observable<any>;
+  errorMessage! : string;
+  constructor(private customerService:CustomerService) { }
+
+  ngOnInit(): void {
+    this.customers = this.customerService.getCustomers();
+  }
+
+}
